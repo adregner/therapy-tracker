6fda5d579a65b91c2f1de6b8c5167d3f8d3b3f39
Andrew Regner <andrew@aregner.com>
2012-02-25 15:19:58 -0600

    first commit, bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Therapy::Application.config.secret_token = '96ee1a9fe4797ba1ffbaf715e274bd119666f8f9d1f51f3f107f926b5504f173568820f36e8357dab8dc34a70e590f49937430a7da61be15b6e2724ce82a8475'
